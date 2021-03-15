2c31d078785f5ab335c84b22be1a205e0bbad288
Yuri Moura <y2moura@hotmail.com>
2021-03-15 18:11:28 -0300

    Adicionado script de render do AfterEffects

---
+++ robots/after-effects/after-effects.js
@@ -0,0 +1,7 @@
+var c = {
+  "height": 1920,
+  "width": 1080,
+  "aspect_ratio": 1,
+  "duration": 508,
+  "framerate": 60
+}
---
+++ robots/after-effects/importAndCreateVideo.js
@@ -0,0 +1,33 @@
+﻿main();
+function main() {
+    app.beginUndoGroup("Main");
+        var folder = new Folder("C:/Users/y2mou/OneDrive/Documentos/twitch-videomaker/robots/videos/");
+        var files = folder.getFiles();
+        var importedItems = [];
+        for(var i = 0; i < files.length; i++) {
+            if(files[i].name.indexOf(".mp4") != -1) {
+                importedItems.push(app.project.importFile(new ImportOptions(files[i])));
+                }
+            }
+      createVideo(importedItems);
+    app.endUndoGroup();
+
+    aep_template_path = "C:/Users/y2mou/OneDrive/Documentos/twitch-videomaker/robots/after-effects/template.aep";
+    app.project.save(File(aep_template_path));
+    }
+
+
+function createVideo(items) {
+    $.evalFile("C:/Users/y2mou/OneDrive/Documentos/twitch-videomaker/robots/after-effects/after-effects.js");
+    var comp = app.project.items.addComp('Main',  c.height,  c.width, 1 , (c.duration+items.length), c.framerate); 
+    var layers = [];
+    var start = 0;
+    for(var i = 0; i < items.length; i++) {
+        layers.push(comp.layers.add(items[i]));
+        layers[i].selected = true;
+        layers[i].startTime = start;
+        start += layers[i].outPoint - layers[i].inPoint;
+        }
+    comp.workAreaDuration = layers[layers.length-1].outPoint;
+    comp.openInViewer();
+}
